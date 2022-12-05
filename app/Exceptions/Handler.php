43593cab80e9ff64bd785be32ef11e3a7d710a02
mostnour <mostnour@yahoo.com>
2022-12-05 16:40:46 +0200

    second commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,8 @@
+n| RequestID = s_fbc8371c-27ed-4228-8dcc-a42838f43e14 
+2022-12-05 15:50:00.9421|ERROR|20|FailedToDecryptConfigInformation: Dsn| RequestID = s_ae7d5868-4380-4792-812a-eab355bb7ff8 
+2022-12-05 15:50:10.9748|ERROR|20|FailedToDecryptConfigInformation: Dsn| RequestID = s_5c0929cd-91ef-485a-8149-7dea70e389a4 
+2022-12-05 15:50:21.0333|ERROR|20|FailedToDecryptConfigInformation: Dsn| RequestID = s_f375744f-0197-459b-9a12-df3363bfacab 
+2022-12-05 15:50:31.0859|ERROR|20|FailedToDecryptConfigInformation: Dsn| RequestID = s_3f24d56b-8e85-41ee-a77a-64d65037b27c 
+2022-12-05 15:50:41.1356|ERROR|17|FailedToDecryptConfigInformation: Dsn| RequestID = s_7da26142-e4f8-464c-87ce-eb32e27df0e2 
+2022-12-05 15:50:51.1905|ERROR|17|FailedToDecryptConfigInformation: Dsn| RequestID = s_d206993a-7641-4657-9ed8-35878131d5e4 
+2022-12-05 1
---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,25 @@
+<?php
+
+use Monolog\Handler\NullHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default log channel that gets used when writing
+    | messages to the logs. The name specified in this option should match
+    | one of the channels defined in the "channels" configuration array.
+    |
+    */
+
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deprecations Log Channel
+    |-----------
